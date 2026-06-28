71a7dbfafa3a07909d7fc2ea0ffc1d7ab43c4636
Taha-Elmkhayer <taha.elmkhayer@gmail.com>
2026-06-28 01:44:09 +0100

    first step of a billion dollar startup

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body bgcolor='gray'>
+        <h1>Hello World</h1>
+        <p>boogo gaga is baller</p>
+    </body>
+</html>
